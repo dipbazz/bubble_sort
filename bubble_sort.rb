63208de9e61805758e90fea5e20c9798b066b8b5
Dipesh Bajgain <dipbazz@gmail.com>
2020-10-22 14:24:33 +0545

    Refactor code to method.
    - created bubble_sort method and placed the logical code of bubble sort there
    - created a swap method to swap the two values.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,17 +1,22 @@
 # frozen_string_literal: true
 
-num_arry = [1, 6, 4, 3, 8, 9, 2, 5, 2, 13, 12, 16, 8]
-
-num_arry.length.times do
-  num_arry.each_with_index do |value, index|
-    next unless index < num_arry.length - 1
-
-    next unless value > num_arry[index + 1]
-
-    temp = value
-    num_arry[index] = num_arry[index + 1]
-    num_arry[index + 1] = temp
-  end
+def swap(first, second)
+  return second, first
 end
 
-puts num_arry
+def bubble_sort(num_arry)
+  num_arry.length.times do
+    num_arry.each_with_index do |value, index|
+      next unless index < num_arry.length - 1
+
+      next unless value > num_arry[index + 1]
+
+      num_arry[index], num_arry[index+1] = swap(value, num_arry[index+1])
+
+    end
+  end
+
+  return num_arry
+end
+
+p bubble_sort([10, 6, 4, 3, 8, 9, 2, 5, 2, 13, 12, 16, 8])
